aec6c00b900d259e9de5c153852518d60c569921
Antoine Leleu <antoine.leleu@acelys.fr>
2021-02-24 15:17:38 +0100

    WIP

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use App\DTO\ContactDTO;
 use App\Form\ContactType;
-use App\Message\SendMail;
+use App\Mailer\Mailer;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -23,7 +23,7 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function contact(Request $request, SendMail $mailer): Response
+    public function contact(Request $request, Mailer $mailer): Response
     {
         $contact = new ContactDTO();
 
